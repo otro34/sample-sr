c25bda976aac9dfd0182ee8e56f629b3e22f1fb2
Juan Romaina <juan.romaina@tranzact.net>
2020-09-18 14:36:34 -0500

    perf: making the component use a span

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <span className={styles.test}>Example Component: {text}</span>
+  return <div className={styles.test}>Example Component: {text}</div>
 }
